8023fb323e0ca4b8b64b4c50e9923806d0462c3e
adizelkin-dotcom <adi.zelkin@gmail.com>
2026-01-31 17:15:53 +0200

    finished

--- scripts/code.py
+++ scripts/code.py
@@ -3,12 +3,11 @@ file=open("scripts/holder.gitkeep","r")
 fileW=open("scripts/mutated_p53.fasta","w")
 
 def Mutate_DNA(seq):
-    mut=""
-    for j in range (3):
+     mut=""
      place = random.randint(0,len(seq)-1)
      nuc=random.randint(1,4)
      for i in range (len(seq)-1):
-      if seq[i,i+2]=="UAA" or seq[i,i+2]=="UAG" or seq[i,i+2]=="UGA":
+      if seq[i:i+2]=="UAA" or seq[i:i+2]=="UAG" or seq[i:i+2]=="UGA":
          break
       if i==place and nuc !=seq[i]:
         if nuc == 1:
@@ -21,18 +20,24 @@ def Mutate_DNA(seq):
             mut+="G"
       else:
        mut+=seq[i] 
-    return mut   
+     return mut   
 
 seq=""
 for line in file:
    seq+=line
-
-
+for i in range (3):
+   seq=Mutate_DNA(seq)
 fileW.write("the original:")
+fileW.write("\n")
 fileW.write(seq)
-fileW.write("the muted:")
+fileW.write("\n")
+fileW.write("the mutated:")
+fileW.write("\n")
 fileW.write(Mutate_DNA(seq))
-if len(Mutate_DNA(seq))<seq:
+fileW.write("\n")
+if len(Mutate_DNA(seq))<len(seq):
    fileW.write("got smaller")
-elif len(Mutate_DNA(seq))==seq:
-   fileW.write("stayd same length")
+else:
+ fileW.write("stayd same length")
+file.close()
+fileW.close()
