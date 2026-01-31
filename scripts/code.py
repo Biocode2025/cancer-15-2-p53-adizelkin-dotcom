0845b355bda091769e37329cadc41c050d16548e
adizelkin-dotcom <adi.zelkin@gmail.com>
2026-01-31 14:20:56 +0200

    שעתוק ותרגום

--- scripts/code.py
+++ scripts/code.py
@@ -1,1 +1,22 @@
-print("Hello")
+import random
+file=open("scripts/holder.gitkeep","r")
+def Mutate_DNA(seq):
+    mut=""
+    p=0
+    for j in range (3):
+     place = random.randint(p,len(seq)-1)
+     nuc=random.randint(1,4)
+     for i in range (len(seq)):
+      if i==place and nuc !=seq[i]:
+        if nuc == 1:
+            mut+="A"
+        elif nuc == 2:
+            mut+="T"
+        elif nuc == 3:
+            mut+="C"
+        elif nuc == 4:
+            mut+="G"
+      else:
+       mut+=seq[i] 
+      p=i
+    return mut   
